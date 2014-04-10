6b4c69939a7a0f9aaec687f42c40b4138c99ab61
shishircse06 <shishir_ruet@yahoo.com>
2014-04-10 15:00:44 +0600

    adding userclass to testing first

---
+++ User.php
@@ -0,0 +1,19 @@
+<?php
+
+class User{
+	public $name;
+	public $email;
+	private $password;
+
+	public function get_password(){
+
+		return $this->$passord;
+	}
+	public function set_password($pass){
+		$this->password=$pass;
+	}
+
+
+
+
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//phpinfo();
+
+require_once('User.php');
+$usr=new User();
+echo $usr->name;
+
+?>
